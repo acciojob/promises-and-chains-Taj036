36d5d586b92664614d3fe673d6c91e7ff9baf83d
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-22 14:12:27 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function canVote() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let name = document.getElementById('name').value;
+      let age = document.getElementById('age').value;
+
+      if (name !== '' && !isNaN(age) && age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else if (name !== '' && !isNaN(age) && age < 18) {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      } else {
+        reject('Something went wrong');
+      }
+    }, 4000);
+  });
+}
+
+document.getElementById('myForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  canVote()
+    .then((data) => {
+      alert(data);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
